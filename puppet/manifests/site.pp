a6386eb3c9d201dc7df69bfdfdb27b0951a96e9d
Vyacheslav Artyukhov <Vyacheslav.Artyukhov@megafon.ru>
2014-06-18 12:39:06 +0400

    change configuration

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -60,24 +60,7 @@ node 'vm-cluster-node1.localdomain' inherits default {
         use_parcels    => true,
   } 
   ->   
-  class { 'cloudera::cm::server': }  
-  ->
-  file{'/var/lib/cloudera':
-    ensure => directory,
-  }
-  ->
-  file{'/var/lib/cloudera/cloudera-services-installer.zip':
-    ensure => file,
-    source => 'puppet:///modules/hadoop/cloudera-services-installer.zip',
-  }
-  ->
-  exec { 'unzip /var/lib/cloudera/cloudera-services-installer.zip -d /var/lib/cloudera/':
-    path => ['/usr/bin'],
-  }
-  ->
-  exec {'java -cp /var/lib/cloudera/lib/cloudera-services-installer-1.0-SNAPSHOT.jar cloudera.services.installer.Main':
-    path => ['/usr/java/default', '/usr/bin'],
-  }
+  class { 'cloudera::cm::server': }    
 }
 
 node 'vm-cluster-node2.localdomain' inherits default {
@@ -98,6 +81,22 @@ node 'vm-cluster-node3.localdomain' inherits default {
 
 #Postgres node, no hadoop
 node 'vm-cluster-node4.localdomain' inherits default {  
+  file{'/var/lib/cloudera':
+    ensure => directory,
+  }
+  ->
+  file{'/var/lib/cloudera/cloudera-services-installer.zip':
+    ensure => file,
+    source => 'puppet:///modules/hadoop/cloudera-services-installer.zip',
+  }
+  ->
+  exec { 'unzip /var/lib/cloudera/cloudera-services-installer.zip -d /var/lib/cloudera/':
+    path => ['/usr/bin'],
+  }
+  ->
+  exec {'java -cp /var/lib/cloudera/lib/cloudera-services-installer-1.0-SNAPSHOT.jar cloudera.services.installer.Main':
+    path => ['/usr/java/default', '/usr/bin'],
+  }
 }
 
 
