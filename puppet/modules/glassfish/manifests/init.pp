d9a604a04fb2fcbdb6f8b7efcef91fb58f4e0699
Vyacheslav Artyukhov <Vyacheslav.Artyukhov@megafon.ru>
2014-07-09 11:58:32 +0400

    disable glassfish console update

--- puppet/modules/glassfish/manifests/init.pp
+++ puppet/modules/glassfish/manifests/init.pp
@@ -30,10 +30,14 @@ class glassfish (
     if $disable_console_updates {
         exec {'/bin/mv /opt/glassfish-web/glassfish/modules/console-updatecenter-plugin.jar /opt/glassfish-web/glassfish/modules/console-updatecenter-plugin.jar.disabled':
             creates => '/opt/glassfish-web/glassfish/modules/console-updatecenter-plugin.jar.disabled',
+            require => Package[$package_name],
+            notify => Service[$package_name],            
         }
     }else {
         exec {'/bin/mv /opt/glassfish-web/glassfish/modules/console-updatecenter-plugin.jar.disabled /opt/glassfish-web/glassfish/modules/console-updatecenter-plugin.jar':
             creates => '/opt/glassfish-web/glassfish/modules/console-updatecenter-plugin.jar',
+            require => Package[$package_name],
+            notify => Service[$package_name],
         }
     }
 
