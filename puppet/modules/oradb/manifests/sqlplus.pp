56302f661a031d7491c7d949fbd8d6a30ac964d9
Vyacheslav Artyukhov <user@email.com>
2014-09-16 11:06:26 +0400

    automate oracle

--- puppet/modules/oradb/manifests/sqlplus.pp
+++ puppet/modules/oradb/manifests/sqlplus.pp
@@ -8,5 +8,6 @@ define oradb::sqlplus(
     environment => ['ORACLE_BASE=/oracle', 'ORACLE_HOME=/oracle/product/11.2/db', 'ORACLE_UNQNAME=TEST', 'ORACLE_SID=test', 'NLS_LANG=AMERICAN_AMERICA.CL8ISO8859P5', "NLS_DATE_FORMAT='DD.MM.YYYY'"],
     command => "sqlplus -s \"/as sysdba\" <<< \"$command\"",
     user => 'oracle',
+    logoutput => true,
   }
 }
